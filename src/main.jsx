e997a800d26176296cc2d557b995fb245118a647
Mohit Singh <mohitkumarsingh9889@gmail.com>
2025-07-12 15:46:04 +0530

    next

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import rootReducer from "./reducer/index.js";
 import { configureStore } from "@reduxjs/toolkit";
@@ -15,10 +15,10 @@ const store = configureStore({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
         <ToastContainer/>
-      </BrowserRouter>
+      </Router>
     </Provider>
   </StrictMode>
 );
